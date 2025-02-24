5cfad7156d77092563c52f86c148e9c59c5f27df
JoshuAmaya1287 <jagustin@hastings.edu>
2025-02-23 23:07:18 -0600

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// name: Joshua Amaya, Email: joshuamaya@unomaha.edu
 #include<iostream>  
   
 using namespace std; 
